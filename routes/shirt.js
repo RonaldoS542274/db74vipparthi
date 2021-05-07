9de981d5857a7dc8c901cc92cf7e0c4708635ef3
RonaldoS542274 <77812398+RonaldoS542274@users.noreply.github.com>
2021-05-06 19:23:38 -0500

    protected the end point

--- routes/shirt.js
+++ routes/shirt.js
@@ -1,20 +1,28 @@
 var express = require('express');
-const shirt_controlers= require('../controllers/shirt');
-
-
+const shirt_controllers= require('../controllers/shirt');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
 
 /* GET shirt */
-router.get('/', shirt_controlers.shirt_view_all_Page);
+router.get('/', shirt_controllers.shirt_view_all_Page);
 
 /* GET detail costume page */
-router.get('/detail', shirt_controlers.shirt_view_one_Page);
+router.get('/detail', shirt_controllers.shirt_view_one_Page);
 
 /* GET create costume page */
-router.get('/create', shirt_controlers.shirt_create_Page);
+router.get('/create', shirt_controllers.shirt_create_Page);
 
 /* GET create update page */
-router.get('/update', shirt_controlers.shirt_update_Page);
+router.get('/update', secured, shirt_controllers.shirt_update_Page);
 
 
 module.exports = router;
