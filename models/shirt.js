260deb00a6ed84c8e6a71151369efa462666e29b
RonaldoS542274 <77812398+RonaldoS542274@users.noreply.github.com>
2021-05-06 20:16:42 -0500

    authentication

--- models/shirt.js
+++ models/shirt.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose")
 const shirtSchema = mongoose.Schema({
 name: String,
-brand: String,
-cost: Number
+brand: {
+       type: String,
+       minLength: 3,
+       },
+cost: {
+    type:Number,
+    min:10,
+    max:100
+}
 })
 module.exports = mongoose.model("shirt", shirtSchema)
